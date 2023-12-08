b2837d9e28fc6e9fd321254061a7b22199fab7d6
Benj <benjani@benj.com>
2023-12-08 18:14:29 +0000

    bob

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Benjani from './pages/benjani'
 import Index from './pages';
-import SignIn from './pages/signin';
 
 function App() {
   return (
