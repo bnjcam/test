bc430c5b4e15e0c60c2f81fc3bf9765136eb9eb5
Benj <benjani@benj.com>
2023-12-13 22:34:29 +0000

    hi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import { HashRouter, Route } from 'react-router-dom';
 import './App.css';
 import Index from './pages';
-import Benjani from './pages/benjani'
 
 function App() {
   return (
-   <HashRouter>
-    <Route path='/' component={<Index />} />
-    <Route path='/benjani' component={<Benjani />} />
-   </HashRouter>
+   <div>
+    <Index />
+   </div>
   );
 }
 
